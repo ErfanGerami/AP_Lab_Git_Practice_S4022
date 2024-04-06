dfbb9eba96aaf312c9e1878813a16c5047a8efdb
mbakh84 <mbakh84@gmail.com>
2024-04-07 00:23:08 +0330

    stage 1

--- 1/1.cpp
+++ 1/1.cpp
@@ -7,22 +7,30 @@ using namespace std;
 class Person {
 public:
 	
+	Person(){};
 	Person(const string& name) {
 		this->name = name;
 
 	}
-	string& getName()  { return name; }
+
+	string& getName() { return name; }
+	const string& getName() const { return name; }
+
+	friend ostream& operator<<(ostream& out, const Person& p);
+
+
 
 private:
 	string name;
 	int age;
-
 };
 
-ostream& operator<<(ostream& out, const Person& p) {
-	cout << p.getName();
+ostream& operator<<(ostream& out, const Person& p) 
+{
+	out << p.getName();
 	return out;
 }
+
 
 
 
@@ -71,33 +79,33 @@ bool isGroupFull(Group g) {
 
 int main()
 {
-	/*
+	
 	Person p1("somename");
-	Person p1("somename2");
+	Person p2("somename2");
 	cout << p1<<p2;
-	*/
-	/*
+	
+	
 	Group g1(5);
 	g1.add({ "Erfan",20 });
 	g1.add({ "Saba",20 });
 	g1.add({ "Mahrokh",20 });
 	g1.add({ "Yasin",21 });
 	cout<<isGroupFull(g1)<<endl;
-	*/
-	/*
+	
+	
 	Group g2(g1);
 	Group g3(10);
 	g2.deletePerson("Erfan");
 	cout << g1<<endl;//should cout every person in it with a \t as the separator
 	cout << g2 << endl;
-	*/
-	/*
+	
+	
 	g3 = g1;
 	cout << g3;
 	g3 =  g2 + g3;//adds members of g2 and g3 to each other
 	g3 += g3 += g3 += g3;
-	*/
-	/*
+	
+	
 	Person persons[10];
 	Person Erfan("Erfan",20);
 	persons[1]=Erfan;
@@ -108,18 +116,8 @@ int main()
 
 	for(int i=0;i<10;i++){
 		cout<<persons[i].getName()<<' ';
-	}*/
-	/*
-	cout<<*find(persons,persons+10,Erfan);
-	*/
+	}
 	
-
-
-
-
-
-
-
-
+	cout<<*find(persons,persons+10,Erfan);
 }
 
