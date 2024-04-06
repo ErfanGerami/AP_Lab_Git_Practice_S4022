07706ac28cdf97e6a7952472fb9cf5c48eefde43
Yalda Jafari <yaldajafari383@gmail.com>
2024-04-06 06:49:33 -0700

    first commit

--- 1/1.cpp
+++ 1/1.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
+class Group;
 class Person {
 public:
 	
@@ -11,12 +11,29 @@ public:
 		this->name = name;
 
 	}
+	Person(const string& name , int const _age) {
+		this->name = name;
+		this->age = _age;
+
+	}
+	Person() : name(" ") , age(0) {}
+
 	string& getName()  { return name; }
+
+	const string& getName() const { return name; }
+
+	void operator = (Person const p) {
+		this->name = p.name;
+		this->age = p.age;
+
+	}
 
 private:
 	string name;
 	int age;
 
+	friend Group;
+	
 };
 
 ostream& operator<<(ostream& out, const Person& p) {
@@ -33,6 +50,11 @@ public:
 		this->cap = max_lenght;
 		this->members = new Person[max_lenght];
 		this->size = 0;
+	}
+	Group() {
+		this->size = 0;
+		this->cap = 0;
+		this->members = NULL;
 	}
 	void add(Person p) {
 		if (this->size == this->cap) {
@@ -54,6 +76,34 @@ public:
 		size--;
 	}
 
+	Group operator + (Group const p) {
+		Group tmp;
+
+		tmp.cap = this->cap + p.cap;
+		tmp.size = this->size + p.size;
+		tmp.members = new Person[tmp.cap];
+
+		for (int i = 0; i < this->size; i++) {
+			tmp.members[i] = this->members[i];
+		}
+
+		for (int i = this->size; i < p.size; i++) {
+			tmp.members[i] = p.members[i];
+		}
+
+	}
+
+	void operator = (Group const g) {
+		this->size = g.size;
+		this->cap = g.cap;
+		this->members = new Person[cap];
+
+		for (int i = 0; i < this->size; i++) {
+			this->members[i] = g.members[i];
+		}
+
+	}
+
 	~Group() {
 		delete[] members;
 	}
@@ -63,7 +113,21 @@ private:
 	int cap;
 	Person* members;
 
+	friend bool isGroupFull(Group g);
+	friend ostream& operator<<(ostream& out, Group& p);
+
 };
+
+ostream& operator<<(ostream& out, Group& p) {
+	Person* ptmp = p.members;
+
+	for (int i = 0; i < p.size ; i++) {
+		cout << p.members[i] << endl;
+	}
+
+
+	return out;
+}
 
 bool isGroupFull(Group g) {
 	return (g.size == g.cap);
@@ -71,33 +135,32 @@ bool isGroupFull(Group g) {
 
 int main()
 {
-	/*
 	Person p1("somename");
-	Person p1("somename2");
+	Person p2("somename2");
 	cout << p1<<p2;
-	*/
-	/*
+	
+	
 	Group g1(5);
 	g1.add({ "Erfan",20 });
 	g1.add({ "Saba",20 });
 	g1.add({ "Mahrokh",20 });
 	g1.add({ "Yasin",21 });
 	cout<<isGroupFull(g1)<<endl;
-	*/
-	/*
+	
+	
 	Group g2(g1);
 	Group g3(10);
 	g2.deletePerson("Erfan");
 	cout << g1<<endl;//should cout every person in it with a \t as the separator
 	cout << g2 << endl;
-	*/
-	/*
+	
+	
 	g3 = g1;
 	cout << g3;
 	g3 =  g2 + g3;//adds members of g2 and g3 to each other
 	g3 += g3 += g3 += g3;
-	*/
-	/*
+	
+	
 	Person persons[10];
 	Person Erfan("Erfan",20);
 	persons[1]=Erfan;
@@ -108,10 +171,10 @@ int main()
 
 	for(int i=0;i<10;i++){
 		cout<<persons[i].getName()<<' ';
-	}*/
-	/*
+	}
+	
 	cout<<*find(persons,persons+10,Erfan);
-	*/
+	
 	
 
 
