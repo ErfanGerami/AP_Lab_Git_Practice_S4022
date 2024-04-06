fa3c1078e84dbfb938e749264a9106aeee7f1b5a
meisampw1384 <mpwryamehr585@gmail.com>
2024-04-06 21:51:09 +0330

    add copy constructor to the Group and fix operators a little bit

--- 1/1.cpp
+++ 1/1.cpp
@@ -5,160 +5,195 @@
 #include <algorithm>
 using namespace std;
 
-class Person {
+class Person
+{
 public:
-	
-	Person(const string& name) {
+	bool operator==(const Person &other) const
+	{
+		return (name == other.name && age == other.age);
+	}
+	bool operator<(const Person &other) const
+	{
+		return name < other.name;
+	}
+	Person(const string &name)
+	{
 		this->name = name;
-
 	}
-	Person (const string& _name,const int& _age):age(_age),name(_name){
+	Person(const string &_name, const int &_age) : age(_age), name(_name)
+	{
 	}
-	Person():name(""),age(0){}
-	string& getName(){ return name; }
-	string getName()const {return name;}
+	Person() : name(""), age(0) {}
+	Person(const Person &other) : name(other.name), age(other.age) {}
+	string &getName() { return name; }
+	string getName() const { return name; }
 	friend class Group;
+
 private:
 	string name;
 	int age;
-
 };
 
-ostream& operator<<(ostream& out, const Person& p) {
+ostream &operator<<(ostream &out, const Person &p)
+{
 	cout << p.getName();
 	return out;
 }
 
-
-
-class Group {
+class Group
+{
 public:
 	friend bool isGroupFull(Group g);
-	Group(int max_lenght)  {
+	Group(int max_lenght)
+	{
 		this->cap = max_lenght;
 		this->members = new Person[max_lenght];
 		this->size = 0;
 	}
-	int get_size(){
+	// I add an copy constructor for g2(g3)
+	Group(const Group &other) : size(other.size), cap(other.cap), members(new Person[other.cap])
+	{
+		for (int i = 0; i < other.size; ++i)
+		{
+			this->members[i] = other.members[i];
+		}
+	}
+
+	int get_size()
+	{
 		return this->size;
 	}
-	int get_cap(){
+	int get_cap()
+	{
 		return this->cap;
 	}
-	void add(Person p) {
-		if (this->size == this->cap) {
+	void add(Person p)
+	{
+		if (this->size == this->cap)
+		{
 			return;
 		}
 		this->members[size] = p;
 		this->size++;
 	}
-	void deletePerson(string name) {
+	void deletePerson(string name)
+	{
 		int i;
-		for (i = 0; i < size; i++) {
-			if(members[i].name == name)
+		for (i = 0; i < size; i++)
+		{
+			if (members[i].name == name)
 				break;
-			
 		}
-		for (int index = i; index < size - 1; index++) {
+		for (int index = i; index < size - 1; index++)
+		{
 			members[index] = members[index + 1];
 		}
 		size--;
 	}
 
-	~Group() {
+	~Group()
+	{
 		delete[] members;
 	}
-	friend ostream& operator<<(ostream& out, const Group& g);
-	Group operator+(const Group & other){
-		int max_of_member=this->cap+other.cap;
-		Group temp(max_of_member);
-		temp.cap=other.cap+this->cap;
-		temp.size=other.size+this->size;
-		int i;
-		for (i=0;i<this->size;i++){
-			temp.members[i]=this->members[i];
+
+	friend ostream &operator<<(ostream &out, const Group &g);
+	Group& operator+(const Group &other)
+	{
+		int newCapacity = this->cap + other.cap;
+		Group result(newCapacity);
+
+		for (int i = 0; i < this->size; ++i)
+		{
+			result.members[result.size++] = this->members[i];
 		}
-		for (int j=i;j<other.size;j++){
-			temp.members[j]=other.members[j];
+
+		for (int i = 0; i < other.size; ++i)
+		{
+			result.members[result.size++] = other.members[i];
 		}
-		return temp;
+		return result;
 	}
-	Group& operator+=(const Group & other){
-		int max_of_member=this->cap+other.cap;
-		Group temp(max_of_member);
-		temp.cap=other.cap+this->cap;
-		temp.size=other.size+this->size;
-		int i;
-		for (i=0;i<this->size;i++){
-			temp.members[i]=this->members[i];
+
+	Group &operator+=(const Group &other)
+	{
+		if (this->size + other.size > this->cap)
+		{
+			int newCapacity = this->size + other.size;
+			Person *temp = new Person[newCapacity];
+			for (int i = 0; i < this->size; ++i)
+			{
+				temp[i] = this->members[i];
+			}
+			delete[] this->members;
+			this->members = temp;
+			this->cap = newCapacity;
 		}
-		for (int j=i;j<other.size;j++){
-			temp.members[j]=other.members[j];
+
+		for (int i = 0; i < other.size; ++i)
+		{
+			this->members[this->size++] = other.members[i];
 		}
-		(*this)=temp;
+
 		return *this;
 	}
 
 private:
 	int size;
 	int cap;
-	Person* members;
-
+	Person *members;
 };
-ostream& operator<<(ostream& out, const Group& g) {
-	out << "the size:"<< g.size;
-	out<< "\n the cap:"<<g.cap;
+ostream &operator<<(ostream &out, const Group &g)
+{
+	out << "the size:" << g.size;
+	out << "\n the cap:" << g.cap;
+	for (int i = 0; i < g.size; i++)
+	{
+		out << "\n member" << i + 1 << ":" << g.members[i].getName();
+	}
 	return out;
 }
-bool isGroupFull(Group g) {
+bool isGroupFull(Group g)
+{
 	return (g.size == g.cap);
 }
 
 int main()
 {
-	
+
 	Person p1("somename");
 	Person p2("somename2");
-	cout << p1<<p2;
-	
+	cout << p1 << p2;
+
 	Group g1(5);
-	g1.add({ "Erfan",20 });
-	g1.add({ "Saba",20 });
-	g1.add({ "Mahrokh",20 });
-	g1.add({ "Yasin",21 });
-	cout<<isGroupFull(g1)<<endl;
+	g1.add({"Erfan", 20});
+	g1.add({"Saba", 20});
+	g1.add({"Mahrokh", 20});
+	g1.add({"Yasin", 21});
+	cout << isGroupFull(g1) << endl;
 
 	Group g2(g1);
 	Group g3(10);
 	g2.deletePerson("Erfan");
-	cout << g1<<endl;//should cout every person in it with a \t as the separator
+	cout << g1 << endl; // should cout every person in it with a \t as the separator
 	cout << g2 << endl;
 	g3 = g1;
 	cout << g3;
-	g3 =  g2 + g3;//adds members of g2 and g3 to each other
+	g3 = g2 + g3; // adds members of g2 and g3 to each other
+	int x = 2;
 	g3 += g3 += g3 += g3;
 
 	Person persons[10];
-	Person Erfan("Erfan",20);
-	persons[1]=Erfan;
-	for(int i=0;i<10;i++){
-		persons[i].getName()=to_string(rand()%100);
+	Person Erfan("Erfan", 20);
+	persons[1] = Erfan;
+	for (int i = 0; i < 10; i++)
+	{
+		persons[i].getName() = to_string(rand() % 100);
 	}
-	sort(persons,persons+10);//what operator should be overloaded for sort? :)
+	sort(persons, persons + 10); // what operator should be overloaded for sort? :)
 
-	for(int i=0;i<10;i++){
-		cout<<persons[i].getName()<<' ';
+	for (int i = 0; i < 10; i++)
+	{
+		cout << persons[i].getName() << ' ';
 	}
-	cout<<*find(persons,persons+10,Erfan);
-
-	
-
-
-
-
-
-
-
-
+	cout << *find(persons, persons + 10, Erfan);
 }
-
