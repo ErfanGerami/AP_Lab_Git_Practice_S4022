1ef7098327063fa406e899f4fd231b864fe1a721
Shadi <shadikhansari1384@gmail.com>
2024-04-06 23:16:23 +0330

    Fixing all compiler errors - the only problem left is that I can't run it!

--- 2/Score.cpp
+++ 2/Score.cpp
@@ -1,5 +1,12 @@
 #include "Score.h"
-
+int Score :: ID_generator;
+Score :: Score() :ID(Score::ID_generator)
+{
+	this->score = 0;
+	this->label = BAD;
+	this->detail = "";
+	ID_generator++;
+}
 Score::Score(int score,char* detail, Label label) :ID(Score::ID_generator) {
 	this->score = score;
 	this->label = label;
@@ -7,6 +14,25 @@ Score::Score(int score,char* detail, Label label) :ID(Score::ID_generator) {
 
 	ID_generator++;
 
+}
+Score::Score(int score,const char* detail, Label label) :ID(Score::ID_generator) {
+	this->score = score;
+	this->label = label;
+	this->detail = detail;
+
+	ID_generator++;
+
+}
+Score& Score:: operator =(const Score& obj) 
+{
+	this->score = obj.score;
+	this->label = obj.label;
+	this->detail = obj.detail;
+	return *this;
+}
+void Score :: setID_generator(int _ID)
+{
+	ID_generator = _ID;
 }
 
 
--- 2/Score.h
+++ 2/Score.h
@@ -1,18 +1,24 @@
+#ifndef Sorce_H
+#define Sorce_H
+
 #include <iostream>
 #include <string>
 using namespace std;
 class Score {
 public:
-	friend void print(const Score& score);
-	
+	friend void print(const Score& score); //dont u forget it in cpp
 	typedef enum {
 		GREAT,
 		GOOD,
 		NOTBAD,
 		BAD,
 	}Label;
-
+	Score();
 	Score(int score,  char* detail, Label label);
+	Score(int score,const char* detail, Label label);
+	Score& operator =(const Score& obj);
+	static void setID_generator(int _ID);
+	friend int main();
 private:
 	int score;
 	Label label;
@@ -21,5 +27,5 @@ private:
 	static int ID_generator;
 };
 
-
+#endif
 
