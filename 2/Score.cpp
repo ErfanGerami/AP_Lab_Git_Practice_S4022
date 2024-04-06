09bcdf847f889721c8dd79b301102100fa50b2b6
Mostafa <mostafa00boostan@gmail.com>
2024-04-06 16:15:04 +0330

    add default constructor for score

--- 2/Score.cpp
+++ 2/Score.cpp
@@ -1,5 +1,12 @@
 #include "Score.h"
 
+Score::Score() :ID(Score::ID_generator){
+	this->score = 0;
+	this->label = GREAT;
+	this->detail = "";
+
+	ID_generator++;
+} 
 Score::Score(int score,char* detail, Label label) :ID(Score::ID_generator) {
 	this->score = score;
 	this->label = label;
