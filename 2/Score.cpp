ecbcfb8c5a2a9e05d154a6fcd979b2c9f8b038a0
kiyan <shafiee4608@gmail.com>
2024-04-09 16:38:57 +0330

    debug project 2

--- 2/Score.cpp
+++ 2/Score.cpp
@@ -1,12 +1,42 @@
 #include "Score.h"
+int Score::ID_generator = 0;
+using namespace std;
 
-Score::Score(int score,char* detail, Label label) :ID(Score::ID_generator) {
+Score::Score(int score, char* detail, Label label) :ID(Score::ID_generator) {
 	this->score = score;
 	this->label = label;
 	this->detail = detail;
 
 	ID_generator++;
 
+}
+
+void print(const Score& score){
+	cout << score.score << " -> " << score.detail << " -> " << score.label << endl;
+}
+Score::Score(int score, std::string detail, Label label) :ID(Score::ID_generator) {
+	this->score = score;
+	this->label = label;
+	this->detail = detail;
+
+	ID_generator++;
+
+}
+Score::Score() : ID(Score::ID_generator) {
+	this->score = 0;
+	this->label = BAD;
+	this->detail = "not set yet!";
+
+	ID_generator++;
+}
+void Score::operator=(const Score& s) {
+	score = s.score;
+	detail = s.detail;
+	label = s.label;
+}
+
+void Score::setID_generator(int newID) {
+	ID_generator = newID;
 }
 
 
--- 2/Score.h
+++ 2/Score.h
@@ -1,10 +1,11 @@
+#pragma once
 #include <iostream>
 #include <string>
 using namespace std;
 class Score {
 public:
 	friend void print(const Score& score);
-	
+
 	typedef enum {
 		GREAT,
 		GOOD,
@@ -12,7 +13,13 @@ public:
 		BAD,
 	}Label;
 
-	Score(int score,  char* detail, Label label);
+	Score(int score, std::string detail, Label label);
+	Score();
+	Score(const Score& s);
+	Score(int score, char* detail, Label label);
+	void operator=(const Score& s);
+	static void setID_generator(int newID);
+	friend int main();
 private:
 	int score;
 	Label label;
