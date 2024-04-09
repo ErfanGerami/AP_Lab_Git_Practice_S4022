cafc613c9703329fff6777e8e1d82ff99b41d248
Hamidreza Hassani <h.r.hassani2005@gmail.com>
2024-04-09 17:42:06 +0330

    fix bugs in score.cpp

--- 2/Score.cpp
+++ 2/Score.cpp
@@ -1,12 +1,35 @@
 #include "Score.h"
 
-Score::Score(int score,char* detail, Label label) :ID(Score::ID_generator) {
-	this->score = score;
+int Score::ID_generator = 0;
+
+Score::Score():
+ID(Score::ID_generator)
+{
+	ID_generator++;
+}
+
+Score::Score(int score,char *detail, Label label) : ID(Score::ID_generator)
+{
+    this->score = score;
 	this->label = label;
 	this->detail = detail;
 
 	ID_generator++;
-
 }
 
+Score::Score(int score,const char *detail, Label label) : ID(Score::ID_generator)
+{
+    this->score = score;
+	this->label = label;
+	this->detail = detail;
 
+	ID_generator++;
+}
+
+void Score::setID_generator(int newIdGen){
+		Score::ID_generator = newIdGen;
+}
+
+void print(const Score& score){
+	cout << score.score << " : " << score.detail << endl;
+}
