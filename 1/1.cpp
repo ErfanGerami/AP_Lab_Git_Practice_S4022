dcb3f23e8bc38d9d7f148fa6ad9c1f61db3f5ad7
Amir Moghadam <amrmhd.am@gmail.com>
2024-04-10 16:14:43 +0330

    Two operators was overloaded for class-group ( + and +=)

--- 1/1.cpp
+++ 1/1.cpp
@@ -76,6 +76,21 @@ public:
 	}
 	friend bool isGroupFull(Group g);
 	friend ostream& operator<< (ostream&, const Group&);
+	friend Group operator+ (const Group&, const Group&);
+	Group& operator= (const Group& g) {
+		this->cap = g.cap;
+		this->size = g.size;
+		delete []this->members;
+		this->members = new Person[cap];
+		for (int i = 0; i < g.size; i++) {
+			this->members[i] = g.members[i];
+		}
+		return *this;
+	}
+	Group& operator+= (const Group& g) {
+		*this = *this + g;
+		return *this;
+	}
 private:
 	int size;
 	int cap;
@@ -91,6 +106,17 @@ ostream& operator<< (ostream& out, const Group& g) {
 		cout << g.members[i] << "\t";
 	}
 	return out;
+}
+Group operator+(const Group& g1, const Group& g2) {
+	Group temp(g1.cap + g2.cap);
+	for (int i = 0; i < g1.size; i++) {
+		temp.members[i] = g1.members[i];
+	}
+	for (int i = g1.size; i < g1.size + g2.size; i++) {
+		temp.members[i] = g2.members[i - g1.size];
+	}
+	temp.size = g1.size + g2.size;
+	return temp;
 }
 
 int main()
@@ -115,12 +141,12 @@ int main()
 	cout << g1<<endl;//should cout every person in it with a \t as the separator
 	cout << g2 << endl;
 	
-	/*
+	
 	g3 = g1;
-	cout << g3;
+	cout << g3 <<endl;
 	g3 =  g2 + g3;//adds members of g2 and g3 to each other
 	g3 += g3 += g3 += g3;
-	*/
+	cout << g3;
 	/*
 	Person persons[10];
 	Person Erfan("Erfan",20);
