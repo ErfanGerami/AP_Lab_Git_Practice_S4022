b8f17b1a2d990b59bb36757cbb1aed136ae48493
Mohammad Amin <Mohammad Amin@DESKTOP-CM77LHB>
2024-04-11 15:59:24 +0330

    Codes Changed

--- 2/Score.h
+++ 2/Score.h
@@ -1,18 +1,23 @@
+#pragma once
+#include "SomeRandomDotHFile.h"
 #include <iostream>
 #include <string>
 using namespace std;
 class Score {
 public:
 	friend void print(const Score& score);
-	
 	typedef enum {
 		GREAT,
 		GOOD,
 		NOTBAD,
 		BAD,
 	}Label;
-
-	Score(int score,  char* detail, Label label);
+	Score();
+	Score(int score, char* detail, Label label);
+	Score(int score, string detail, Label label);
+	Score& operator=(const Score& s);
+	static void setID_generator(int id);
+	friend int main();
 private:
 	int score;
 	Label label;
