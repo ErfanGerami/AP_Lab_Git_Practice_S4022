a7b33462b79268e4dc93e0179037b4ad4bede043
Ghazala <ghazal1384926054@gmail.com>
2024-04-13 00:31:54 +0430

    edited

--- 2/Score.cpp
+++ 2/Score.cpp
@@ -1,6 +1,8 @@
 #include "Score.h"
 
-Score::Score(int score,char* detail, Label label) :ID(Score::ID_generator) {
+int Score :: ID_generator;
+
+Score::Score(int score, const char* detail, Label label) :ID(Score::ID_generator) {
 	this->score = score;
 	this->label = label;
 	this->detail = detail;
@@ -9,4 +11,16 @@ Score::Score(int score,char* detail, Label label) :ID(Score::ID_generator) {
 
 }
 
+Score :: Score() : ID(ID_generator) , score(0) , label(GREAT) , detail("") {
+	ID_generator++;
+}
 
+void Score :: setID_generator(int _ID_generator) {
+	ID_generator = _ID_generator;
+}
+
+void :: Score :: operator = (const Score& _score){
+	this->score = _score.score;
+	this->label = _score.label;
+	this->detail = _score.detail;
+}
