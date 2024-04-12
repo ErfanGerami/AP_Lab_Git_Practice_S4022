374581281e4173345489f2a251e8fb68ea795685
maryam <mar.mar.2004me@gmail.com>
2024-04-12 15:48:25 +0330

    fix problems

--- 2/Score.cpp
+++ 2/Score.cpp
@@ -8,27 +8,30 @@ Score::Score(int score,char* detail, Label label) :ID(Score::ID_generator) {
 	ID_generator++;
 
 }
+Score::Score():ID(ID_generator) {
+        label = GOOD;
+		score = 0;
+		detail = " ";
+		ID_generator++;
+}
 Score::Score(int s, string d, Label l):ID(ID_generator) {
 		this->score = s;
 		this->detail = d;
 		this->label = l;
 		ID_generator ++;
 
-	}
-Score::Score():ID(ID_generator) {
-		score = 0;
-		detail = "";
-		label = NOTBAD;
-		ID_generator++;
-	}
+}
 Score& Score::operator= (const Score& s) {
 		score = s.score;
 		detail = s.detail;
 		label = s.label;
 		return *this;
-	}
+}
 void print(const Score& s) {
-	cout << "Score: " << s.score << endl << "Detail: " << s.detail << endl << "Lable: " << s.label << endl << "ID: " << s.ID << endl;
+	cout << "Score: " << s.score << endl;
+    cout << "Detail: " << s.detail << endl;
+    cout << "Lable: " << s.label << endl;
+    cout << "ID: " << s.ID << endl;
 }
 void setID_generator(int id) {
 	Score::ID_generator = id;
