7e1465af451bd7669713d22b6c93d05fdd6f829e
maryam <mar.mar.2004me@gmail.com>
2024-04-12 15:40:40 +0330

    fix bugs

--- 2/Score.cpp
+++ 2/Score.cpp
@@ -1,12 +1,38 @@
 #include "Score.h"
+int Score::ID_generator = 0;
 
 Score::Score(int score,char* detail, Label label) :ID(Score::ID_generator) {
 	this->score = score;
 	this->label = label;
 	this->detail = detail;
-
 	ID_generator++;
 
+}
+Score::Score(int s, string d, Label l):ID(ID_generator) {
+		this->score = s;
+		this->detail = d;
+		this->label = l;
+		ID_generator ++;
+
+	}
+Score::Score():ID(ID_generator) {
+		score = 0;
+		detail = "";
+		label = NOTBAD;
+		ID_generator++;
+	}
+Score& Score::operator= (const Score& s) {
+		score = s.score;
+		detail = s.detail;
+		label = s.label;
+		return *this;
+	}
+void print(const Score& s) {
+	cout << "Score: " << s.score << endl << "Detail: " << s.detail << endl << "Lable: " << s.label << endl << "ID: " << s.ID << endl;
+}
+void setID_generator(int id) {
+	Score::ID_generator = id;
+	return ;
 }
 
 
--- 2/main.cpp
+++ 2/main.cpp
@@ -9,11 +9,9 @@ using namespace std;
 #define FOR(n) for(int i=0;i<n;i++)
 #define SETSCORE(i) int score=rand()%20+1;/*no 0s*/\
 scores[i] = Score( score,("some string " + to_string(i)).c_str(),Score::Label(3-score/5)) ;
-
-
 int main()
 {
-	
+
 	srand(time(NULL));
 
 	Score s1(1, "do better next time", Score::BAD);
@@ -26,7 +24,7 @@ int main()
 	FOR(10) {
 		print(scores[i]);
 	}
-	Score::setID_generator(Score::ID_generator+100);//just a setter for ID_generator
+	setID_generator(Score::ID_generator+100);//just a setter for ID_generator
 
 }
 
