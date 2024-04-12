1d9feb3f878478af48a2b28ed2568d03a820aa7b
unknown <mrjfz13841393@gmail.com>
2024-04-12 11:51:49 +0330

    file 2 fixed

--- 2/Score.cpp
+++ 2/Score.cpp
@@ -1,5 +1,7 @@
 #include "Score.h"
 
+int Score::ID_generator = 0;
+
 Score::Score(int score,char* detail, Label label) :ID(Score::ID_generator) {
 	this->score = score;
 	this->label = label;
@@ -8,5 +10,41 @@ Score::Score(int score,char* detail, Label label) :ID(Score::ID_generator) {
 	ID_generator++;
 
 }
+Score::Score(int score, const char* _detail, Label label) :ID(Score::ID_generator),detail(_detail) {
+	this->score = score;
+	this->label = label;
+	
+	ID_generator++;
+}
+Score::Score(int score, string detail, Label label) :ID(Score::ID_generator) {
+	this->score = score;
+	this->label = label;
+	this->detail = detail;
 
+	ID_generator++;
 
+}
+Score::Score() :ID(Score::ID_generator) {
+	this->score = 0;
+	this->detail = "";
+	ID_generator++;
+}
+
+void Score::setID_generator(int num) { ID_generator = num; }
+
+Score& Score::operator=(const Score& S)
+{
+	this->score = S.score;
+	this->detail = S.detail;
+	this->label = S.label;
+
+	return *this;
+}
+
+void print(const Score& score)
+{
+	cout << endl << "ID      -> " << score.ID << endl;
+	cout << "Score   -> " << score.score << endl;
+	cout << "Lable   -> " << score.label << endl;
+	cout << "Detail  -> " << score.detail << endl;
+}
--- 2/main.cpp
+++ 2/main.cpp
@@ -5,7 +5,9 @@
 #include <algorithm>
 #include "Score.h"
 #include "SomeRandomDotHFile.h"
+
 using namespace std;
+
 #define FOR(n) for(int i=0;i<n;i++)
 #define SETSCORE(i) int score=rand()%20+1;/*no 0s*/\
 scores[i] = Score( score,("some string " + to_string(i)).c_str(),Score::Label(3-score/5)) ;
