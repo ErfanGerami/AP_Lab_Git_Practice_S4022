3a8d67a1b3a6964375fbf5434c4be7c8c50356fd
hosseinoruji1384 <hossein.oruji.1384.5688@gmail.com>
2024-04-12 23:20:44 +0330

    copy constructors created

--- 2/Score.h
+++ 2/Score.h
@@ -1,25 +1,27 @@
+#pragma once
 #include <iostream>
 #include <string>
 using namespace std;
 class Score {
 public:
 	friend void print(const Score& score);
-	
 	typedef enum {
 		GREAT,
 		GOOD,
 		NOTBAD,
 		BAD,
 	}Label;
-
-	Score(int score,  char* detail, Label label);
+	Score(int score, const char* detail, Label label);
+	void static setID_generator(int id_generator);
+	Score& Score::operator=(const Score& p);
+	Score();
+	friend int main();
+	//Score::Score() :score(0), label(Label()), detail(""), ID(Score::ID_generator) {};
+	void print(const Score&);
 private:
+	const int ID;
 	int score;
 	Label label;
 	string detail;
-	const int ID;
 	static int ID_generator;
 };
-
-
-
