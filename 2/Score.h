d8f0642939b1edd0c51d75ed216e138a1fadd630
unknown <mokazemaini84@gmail.com>
2024-04-12 23:24:51 +0330

    first

--- 2/Score.h
+++ 2/Score.h
@@ -1,18 +1,33 @@
+#pragma once
 #include <iostream>
 #include <string>
 using namespace std;
 class Score {
 public:
+	Score() :ID(Score::ID_generator++), score(0), label(GREAT), detail("") {}
 	friend void print(const Score& score);
-	
+	friend int main();
 	typedef enum {
 		GREAT,
 		GOOD,
 		NOTBAD,
 		BAD,
 	}Label;
+	//Score(int score, const char* detail, Label label);
 
-	Score(int score,  char* detail, Label label);
+	Score(int score, char* detail, Label label);
+
+	//Score(int score, string& detail, Label label);
+
+	Score(int score, const char* detail, Label label);
+
+	static void setID_generator(int newId);
+
+	//	Score(int score, char* detail, Label label);
+
+	Score& operator = (const Score& score);
+
+
 private:
 	int score;
 	Label label;
@@ -20,6 +35,8 @@ private:
 	const int ID;
 	static int ID_generator;
 };
+//int Score::ID_generator = 0;
+
 
 
 
