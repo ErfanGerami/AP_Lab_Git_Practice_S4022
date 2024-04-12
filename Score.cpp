8e4782c5b61e3cde20cbbdacd270bd1649735e0b
Erfan3101 <jfryrfan08@gmail.com>
2024-04-12 19:19:11 +0330

    copy constructor

---
+++ Score.cpp
@@ -0,0 +1,12 @@
+#include "Score.h"
+
+Score::Score(int score,char* detail, Label label) :ID(Score::ID_generator) {
+	this->score = score;
+	this->label = label;
+	this->detail = detail;
+
+	ID_generator++;
+
+}
+
+
