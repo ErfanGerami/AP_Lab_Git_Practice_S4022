9c81a3dc530e99404b1cb32680790e4f228255a4
Matin HeidarNejad <mmheidarnejad@gmail.com>
2024-04-12 20:52:59 +0330

    fix all bugs

--- 2/main.cpp
+++ 2/main.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include "Score.h"
 #include "SomeRandomDotHFile.h"
+#include <bits/stdc++.h>
 using namespace std;
 #define FOR(n) for(int i=0;i<n;i++)
 #define SETSCORE(i) int score=rand()%20+1;/*no 0s*/\
@@ -13,7 +14,7 @@ scores[i] = Score( score,("some string " + to_string(i)).c_str(),Score::Label(3-
 
 int main()
 {
-	
+
 	srand(time(NULL));
 
 	Score s1(1, "do better next time", Score::BAD);
