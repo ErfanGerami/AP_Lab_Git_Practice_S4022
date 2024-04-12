51a69242bfab607d117e2070b1da411c3e058f89
sepideh <3pideh.amini66@gmail.com>
2024-04-12 22:22:18 +0330

    everything is okn except Sorce.cpp that I cant find any solution for this. Sorry.

--- 2/Score.cpp
+++ 2/Score.cpp
@@ -1,12 +1,11 @@
 #include "Score.h"
-
-Score::Score(int score,char* detail, Label label) :ID(Score::ID_generator) {
+using namespace std;
+int Score::ID_generator = 0;
+Score :: Score(int score,char* detail, Label label)  : Score::ID(Score::ID_generator) {
 	this->score = score;
 	this->label = label;
 	this->detail = detail;
-
+    
 	ID_generator++;
 
 }
-
-
--- 2/Score.h
+++ 2/Score.h
@@ -1,25 +1,57 @@
+
+#ifndef HEADERFILE_H
+#define HEADERFILE_H
 #include <iostream>
 #include <string>
 using namespace std;
+
 class Score {
 public:
-	friend void print(const Score& score);
-	
-	typedef enum {
-		GREAT,
-		GOOD,
-		NOTBAD,
-		BAD,
-	}Label;
-
-	Score(int score,  char* detail, Label label);
+    friend void print(const Score& score);
+    friend int main ();
+    typedef enum {
+        GREAT,
+        GOOD,
+        NOTBAD,
+        BAD,
+    } Label;
+	// Score() : score(0), label(GOOD), detail(""), ID(ID_generator++) {}
+    Score(int score, const char* detail, Label label); 
+    static void setID_generator(int value) {
+        ID_generator = value;
+    }
+    Score() : score(0), label(GREAT), detail(""), ID(ID_generator) {
+        ID_generator++;
+    }
+    Score(int id) : ID(id) {}
+    Score(const string& detail) : score(0), label(GREAT), detail(detail), ID(ID_generator) {
+    ID_generator++;
+    }
+    Score(const string& detail, Label label) : detail(detail), label(label) ,  ID (ID_generator) {
+        ID_generator ++;
+    }
+    Score& operator=(const Score& other) {
+        // Copy things from other to this
+        // ...
+        return *this;
+    }
 private:
-	int score;
-	Label label;
-	string detail;
-	const int ID;
-	static int ID_generator;
+    int score;
+    Label label;
+    string detail;
+    const int ID;
+    static int ID_generator;
 };
+
+#endif 
+
+
+
+
+
+
+
+
 
 
 
